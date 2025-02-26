00878c265814bfcf9f6c46c8422577241f67788d
SimM20 <83238073+SimM20@users.noreply.github.com>
2025-02-26 17:51:44 -0300

    Translations and minnor changes
    Added almost all traductions and fixed minnor bugs

--- process-form.php
+++ process-form.php
@@ -3,6 +3,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['username']);
     $email = htmlspecialchars($_POST['useremail']);
     $message = htmlspecialchars($_POST['usermessage']);
+    $fileInfo = "None.";
+
+    if (isset($_FILES['userfile']) && $_FILES['userfile']['error'] == 0) {
+        $uploadDir = "uploads/";
+        if (!is_dir($uploadDir)) {
+            mkdir($uploadDir, 0777, true);
+        }
+        $destination = $uploadDir . basename($_FILES['userfile']['name']);
+        if (move_uploaded_file($_FILES['userfile']['tmp_name'], $destination)) {
+            $fileInfo = "File uploaded: " . $destination;
+        } else {
+            $fileInfo = "Error uploading the file.";
+        }
+    }
 
     $to = "simon.moraleslozano@live.com";
     $subject = "Nuevo mensaje de contacto";
@@ -12,6 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $body = "Nombre: $name\n";
     $body .= "Correo: $email\n";
     $body .= "Mensaje:\n$message\n";
+    $body .= "\n$fileInfo";
 
     if (mail($to, $subject, $body, $headers)) {
         echo "Message sent successfully!";
